0abbac674d2fc6ffb20afe63444830b2f1101fbf
arjunkotari <arjun.kotari123@gmail.com>
2021-08-05 22:37:39 +0530

    Added:Email verification

--- src/main/java/com/userRegistration/UserRegistration.java
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -9,8 +9,8 @@ public class UserRegistration {
     //Scanner class to accepting values at execution time.
     Scanner scanner = new Scanner(System.in);
     //Global variables.
-    public static final String userName = "^[A-Z]{1}[a-zA-Z]{2,}+$"; //using Regex
-
+    public static final String NAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2,}+$"; //using Regex
+    public static final String EMAIL_REGEX = "^([a-z0-9]+.)+@([a-z0-9]+.)([a-z]+.)[a-z]{2,3}+$";
     /*
     Validating First Name.
      */
@@ -18,7 +18,7 @@ public class UserRegistration {
         System.out.println("Enter First Name");
         String name = scanner.next();
         //Comparing the name with userName
-        if(name.matches(userName))
+        if(name.matches(NAME_REGEX))
             System.out.println("Valid First Name");
         else
             System.out.println("In Valid First Name");
@@ -30,10 +30,22 @@ public class UserRegistration {
         System.out.println("Enter Last Name");
         String name = scanner.next();
         //Comparing the name with userName
-        if(name.matches(userName))
+        if(name.matches(NAME_REGEX))
             System.out.println("Valid Last Name");
         else
             System.out.println("In Valid Last Name");
+    }
+    /*
+    Validating Email.
+     */
+    public void email(){
+        System.out.println("Enter email");
+        String email = scanner.next();
+        //Comparing the name with email
+        if(email.matches(EMAIL_REGEX))
+            System.out.println("Valid Email");
+        else
+            System.out.println("In Valid Email");
     }
     /*
     Main method starts.
@@ -45,5 +57,6 @@ public class UserRegistration {
         //calling methods
         registration.firstName();
         registration.LastName();
+        registration.email();
     }
 }
