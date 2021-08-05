42338f3325b6960e3270a1fd2acc8b32c9e040a3
arjunkotari <arjun.kotari123@gmail.com>
2021-08-05 22:03:55 +0530

    Added:Welcome Message

---
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.userRegistration;
+
+public class UserRegistration {
+    /*
+    User Registration System.
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration");
+    }
+}
